207e76208f17ad0c590cf5816547f68a41e788dc
ParamiJayathilaka <thinithiparami53@gmail.com>
2024-05-20 10:25:57 +0530

    emp

--- BackEnd (2)/BackEnd/src/main/java/com/example/demo/dto/EmployeeDTO.java
+++ BackEnd (2)/BackEnd/src/main/java/com/example/demo/dto/EmployeeDTO.java
@@ -1,4 +1,41 @@
 package com.example.demo.dto;
 
+
+import com.example.demo.util.AccessRole;
+import com.example.demo.util.Gender;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.sql.Date;
+
+@ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
+
+    private String empCode;
+    private String employeeName;
+    private String employeeProfilePic;
+    private Gender gender;
+    private String status;
+    private String designation;
+    private AccessRole accessRole;
+    private Date dob;
+    private Date dateOfJoin;
+    private String branch;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String addressLine4;
+    private String addressLine5;
+    private String contactNum;
+    private String email;
+    private String emergency;
+    private String emergencyContact;
 }
---
+++ BackEnd (2)/BackEnd/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.CustomerDTO;
+import com.example.demo.dto.EmployeeDTO;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    EmployeeDTO saveEmployee(EmployeeDTO employeeDTO );
+    EmployeeDTO updateEmployee(EmployeeDTO employeeDTO);
+    boolean deleteEmployee(String id);
+    List<EmployeeDTO> getAllEmployee();
+    List<EmployeeDTO> searchEmployee(String name);
+}
