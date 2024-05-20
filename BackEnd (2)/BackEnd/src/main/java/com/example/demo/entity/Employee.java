c2244a1892eba4d52282fe94561fe0101d350984
ParamiJayathilaka <thinithiparami53@gmail.com>
2024-05-20 10:17:08 +0530

    add

--- BackEnd (2)/BackEnd/src/main/java/com/example/demo/entity/Employee.java
+++ BackEnd (2)/BackEnd/src/main/java/com/example/demo/entity/Employee.java
@@ -1,4 +1,49 @@
 package com.example.demo.entity;
 
+import com.example.demo.util.AccessRole;
+import com.example.demo.util.Gender;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+
+    @Id
+    private String empCode;
+    private String employeeName;
+    private String employeeProfilePic;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private String status;
+    private String designation;
+
+    @Enumerated(EnumType.STRING)
+    private AccessRole accessRole;
+    private Date dob;
+    private Date dateOfJoin;
+    private String branch;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String addressLine4;
+    private String addressLine5;
+    private String contactNum;
+    private String email;
+    private String emergency;
+    private String emergencyContact;
+
+
+
 }
